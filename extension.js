b5abcce91b5ffbfbaa4b1a2340b4a4c3c2a991f8
bohaixu <bohaixu@meituan.com>
2021-07-26 11:38:06 +0800

    feat: 添加 ts 文件检测功能逻辑

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const { window, StatusBarAlignment, workspace } = require('vscode')
-const babel = require('@babel/core');
-const { minify } = require("terser");
+const babel = require('@babel/core')
+const { minify } = require("terser")
+const path = require('path')
 
 function activate(context) {
 	const status = window.createStatusBarItem(StatusBarAlignment.Right, 999)
@@ -12,18 +13,21 @@ function activate(context) {
 }
 
 function calculateCurrentCompressedFileSize(editor, status) {
-	if(editor.document.uri.scheme !== 'file' || !/\.js$/.test(editor.document.uri._fsPath)) {
+	// todo 检测 wxml 、wxs、 css 文件
+	if(editor.document.uri.scheme !== 'file' || !(/\.js$/.test(editor.document.uri._fsPath) || /\.ts$/.test(editor.document.uri._fsPath))) {
 		status.hide();
 		return
 	}
 
 	const code = editor.document.getText()
+	const fileNmae = path.basename(editor.document.uri._fsPath)
 
-	compressFile(code, status)
+	compressFile(code, status, fileNmae)
 }
 
-function compressFile(code, status) {
+function compressFile(code, status, fileNmae) {
 	const optionsObject = {
+		filename: fileNmae,
 		presets: [
 			// wxPreset,
 			[
@@ -36,6 +40,7 @@ function compressFile(code, status) {
 					loose: true,
 				},
 			],
+			['@babel/preset-typescript']
 		],
 		comments: false,
 		plugins: [
@@ -96,11 +101,62 @@ function compressFile(code, status) {
 	}).then(res => {
 
 		status.tooltip = '压缩后文件大小单位（B）'
-		status.text = '压缩后文件大小：' + res.code.length
+		status.text = '压缩后文件大小：' + sizeOf(res.code)
 		status.color = '#FFFFFF'
 		status.show();
 	})
 }
+
+/**
+ * UTF-8 是一种可变长度的 Unicode 编码格式，使用一至四个字节为每个字符编码
+ *
+ * 000000 - 00007F(128个代码)      0zzzzzzz(00-7F)                             一个字节
+ * 000080 - 0007FF(1920个代码)     110yyyyy(C0-DF) 10zzzzzz(80-BF)             两个字节
+ * 000800 - 00D7FF
+   00E000 - 00FFFF(61440个代码)    1110xxxx(E0-EF) 10yyyyyy 10zzzzzz           三个字节
+ * 010000 - 10FFFF(1048576个代码)  11110www(F0-F7) 10xxxxxx 10yyyyyy 10zzzzzz  四个字节
+ * UTF-16 大部分使用两个字节编码，编码超出 65535 的使用四个字节
+ * 000000 - 00FFFF  两个字节
+ * 010000 - 10FFFF  四个字节
+ * @param  {String} str
+ * @param  {String} charset utf-8, utf-16
+ * @return {Number}
+ */
+function sizeOf(str, charset = 'utf-8') {
+    let total = 0;
+    let charCode;
+    let i;
+    let len;
+
+    charset = charset ? charset.toLowerCase() : '';
+
+    if (charset === 'utf-16' || charset === 'utf16') {
+        for (i = 0, len = str.length; i < len; i++) {
+            charCode = str.charCodeAt(i);
+
+            if (charCode <= 0xffff) {
+                total += 2;
+            } else {
+                total += 4;
+            }
+        }
+    } else {
+        for (i = 0, len = str.length; i < len; i++) {
+            charCode = str.charCodeAt(i);
+            if (charCode <= 0x007f) {
+                total += 1; // 127]
+            } else if (charCode <= 0x07ff) {
+                total += 2; // 2047]
+            } else if (charCode <= 0xffff) {
+                total += 3; // 65535]
+            } else {
+                total += 4;
+            }
+        }
+    }
+    return total;
+}
+
 
 module.exports = {
 	activate
